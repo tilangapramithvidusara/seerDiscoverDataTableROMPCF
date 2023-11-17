9ab89e0540c6e298c12388ce1dbbf40c10bfea01
user <tilanga.pramith@1billiontech.com>
2023-11-17 09:46:16 +0530

    complexity

--- seerDiscoverDataTableROMPCF/src/Utils/RequirmentData/requirement.data.utils.ts
+++ seerDiscoverDataTableROMPCF/src/Utils/RequirmentData/requirement.data.utils.ts
@@ -1,12 +1,13 @@
-import { fitGapData, moscowsData, percentData } from "../../Constants/pickListData";
+import { complexityData, fitGapData, moscowsData, percentData } from "../../Constants/pickListData";
 
 export const dataMapper = (data: any[]) => {
   try {
-    const finalData = data && data.length && data.map((value: {seer_Moscow:  any, seer_FitGap: any}) => {
+    const finalData = data && data.length && data.map((value: {seer_Moscow:  any, seer_FitGap: any, seer_Complexity: any}) => {
       let ob = {
         ...value, // Copy existing properties
         seer_Moscow: moscowsData[value['seer_Moscow']],
         seer_FitGap: fitGapData[value['seer_FitGap']],
+        seer_Complexity: complexityData[value['seer_Complexity']],
       };
       
       // ob['seer_Moscow'] = moscowsData[value['seer_Moscow']];
