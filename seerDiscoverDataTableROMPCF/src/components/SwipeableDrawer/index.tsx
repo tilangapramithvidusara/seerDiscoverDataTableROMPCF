73a07d8e55fdca8e5e1fd6eba4cfa3b8700cdbb4
anujshashimal1bt <anuj.jayasekara@1billiontech.com>
2023-11-20 11:53:31 +0530

    updated

--- seerDiscoverDataTableROMPCF/src/components/SwipeableDrawer/index.tsx
+++ seerDiscoverDataTableROMPCF/src/components/SwipeableDrawer/index.tsx
@@ -9,18 +9,58 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
+import CostIcon from '@mui/icons-material/MonetizationOn';
+import HourlyRate from '@mui/icons-material/QueryBuilder';
+import Total from '@mui/icons-material/CurrencyExchange';
+
 import MailIcon from '@mui/icons-material/Mail';
+import { useSelector } from 'react-redux';
 
 type Anchor = 'top' | 'left' | 'bottom' | 'right';
 
 export default function SwipeableTemporaryDrawer({
-  anchor, isOpenSideDrawer, data, setIsOpenSideDrawer
+  anchor, isOpenSideDrawer, data, setIsOpenSideDrawer, cellDataForSidePane
 }: {
   anchor: Anchor, 
   isOpenSideDrawer: boolean,
   setIsOpenSideDrawer: any,
-  data: any
-}) {
+  data: any,
+  cellDataForSidePane: any
+  }) {
+  
+  const selectorForSidePane = useSelector((state: any) => state?.sidePane?.data);
+  const [showCellData, setShowCellData] = React.useState<any>();
+  const [sidePanelData, setSidePaneData] = React.useState<any>();
+
+  React.useEffect(() => {
+    console.log("cellDataForSidePane 1", cellDataForSidePane);
+    console.log("cellDataForSidePane 2", selectorForSidePane);
+    
+
+
+    if (cellDataForSidePane?.table === "Estimate Avg Rate") {
+      if (cellDataForSidePane?.nameCategory === "ANALYSIS & DESIGN") {
+        if (cellDataForSidePane?.name === "Analysis and Design") {
+          const datatoShow = selectorForSidePane?.estimageAveRateAnalysisDesignSidePane;
+          setShowCellData(datatoShow[cellDataForSidePane?.columnId?.replace(/\//g, '')]);
+        } else if (cellDataForSidePane?.name === "Customisations (Design)") {
+          const datatoShow = selectorForSidePane?.estimageAveRateCustomisationDesignSidePane;
+          setShowCellData(datatoShow[cellDataForSidePane?.columnId?.replace(/\//g, '')]);
+        } else if (cellDataForSidePane?.name === "Custom Requirements (Design)") {
+          const datatoShow = selectorForSidePane?.estimageAveRateCustomerRequirementDesignSidePane;
+          setShowCellData(datatoShow[cellDataForSidePane?.columnId?.replace(/\//g, '')]);
+        } else if (cellDataForSidePane?.name === "Documentation") {
+          const datatoShow = selectorForSidePane?.estimageAveRateCustomerDocumentationSidePane;
+          setShowCellData(datatoShow[cellDataForSidePane?.columnId?.replace(/\//g, '')]);
+        } else if (cellDataForSidePane?.name === "Design Review") {
+          const datatoShow = selectorForSidePane?.estimageAveRateCustomerDesignReviewSidePane;
+          setShowCellData(datatoShow[cellDataForSidePane?.columnId?.replace(/\//g, '')]);
+        }
+      }
+    }
+  }, [cellDataForSidePane]);
+
+
   const [state, setState] = React.useState({
     top: false,
     left: false,
@@ -53,174 +93,67 @@ export default function SwipeableTemporaryDrawer({
         toggleDrawer(anchor, false)
       }
     >
-      {/* InboxIcon */}
+      <div>
+      </div>
       <List>
-        <ListItem key={'type'} disablePadding>
+        <ListItem key={'cost'} disablePadding>
           <ListItemButton>
             <ListItemIcon>
-              {<MailIcon />}
+              {<CostIcon />}
             </ListItemIcon>
-            <ListItemText primary={`Type: ${data?.original?.type}`} />
+            <ListItemText primary={`Effort: ${showCellData?.subValue}`} />
           </ListItemButton>
         </ListItem>
-        <ListItem key={'nameCategory'} disablePadding>
+        <ListItem key={'hourlyRate'} disablePadding>
           <ListItemButton>
             <ListItemIcon>
-              {<MailIcon />}
+              {<HourlyRate />}
             </ListItemIcon>
-            <ListItemText primary={`Category: ${data?.original?.nameCategory}`} />
+            <ListItemText primary={`Hourly Rate: ${showCellData?.hourlyRate}`}/>
           </ListItemButton>
         </ListItem>
-        <ListItem key={'name'} disablePadding>
+        <ListItem key={'hoursPerDay'} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {<HourlyRate />}
+            </ListItemIcon>
+            <ListItemText primary={`Hours Per Day: ${showCellData?.hoursPerday}`} />
+          </ListItemButton>
+        </ListItem>
+        {/* <ListItem key={'subValue'} disablePadding>
           <ListItemButton>
             <ListItemIcon>
               {<MailIcon />}
             </ListItemIcon>
-            <ListItemText primary={`Name: ${data?.original?.name}`} />
+            <ListItemText primary={`Sub Value: ${showCellData?.subValue}`} />
+          </ListItemButton>
+        </ListItem> */}
+        <ListItem key={'Logic'} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {<CostIcon />}
+            </ListItemIcon>
+            <ListItemText primary={`Logic: ${showCellData?.logic?.toString().replace("Cost", "effort")}`} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={'DataLogic'} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {<CostIcon />}
+            </ListItemIcon>
+            <ListItemText primary={`Data with Logic: ${showCellData?.shown}`} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={'total'} disablePadding style={{color: 'black'}}>
+          <ListItemButton>
+            <ListItemIcon>
+              {<Total />}
+            </ListItemIcon>
+            <ListItemText primary={`Total: ${showCellData?.value}`} />
           </ListItemButton>
         </ListItem>
         <Divider />
-        {
-          (data?.original?.type === 'Estimate Resource' || data?.original?.type === 'Estimate Resource Milestone') ? (
-            <div>
-              <ListItem key={'M'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  <ListItemText primary={'Must'}/>
-                  {/* <ListItemText primary={`M/S/C: ${data?.original?.['M/S/C']}`} /> */}
-                </ListItemButton>
-              </ListItem>
-              <ListItem key={'MR1'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  {/* <ListItemText primary={'Resource 1'}/> */}
-                  <ListItemText primary={`Resource 1: ${data?.original?.['M_Resource1']}`} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem key={'MR2'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  {/* <ListItemText primary={'Resource 1'}/> */}
-                  <ListItemText primary={`Resource 2: ${data?.original?.['M_Resource2']}`} />
-                </ListItemButton>
-              </ListItem>
-              <Divider/>
-              <ListItem key={'M/S'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  <ListItemText primary={'Must Should'}/>
-                  {/* <ListItemText primary={`M/S/C: ${data?.original?.['M/S/C']}`} /> */}
-                </ListItemButton>
-              </ListItem>
-              <ListItem key={'MSR1'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  {/* <ListItemText primary={'Resource 1'}/> */}
-                  <ListItemText primary={`Resource 1: ${data?.original?.['M/S_Resource1']}`} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem key={'MSR2'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  {/* <ListItemText primary={'Resource 1'}/> */}
-                  <ListItemText primary={`Resource 2: ${data?.original?.['M/S_Resource2']}`} />
-                </ListItemButton>
-              </ListItem>
-              <Divider/>
-              <ListItem key={'MSC'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  <ListItemText primary={'Must Should Could'}/>
-                  {/* <ListItemText primary={`M/S/C: ${data?.original?.['M/S/C']}`} /> */}
-                </ListItemButton>
-              </ListItem>
-              <ListItem key={'MSCR1'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  {/* <ListItemText primary={'Resource 1'}/> */}
-                  <ListItemText primary={`Resource 1: ${data?.original?.['M/S/C_Resource1']}`} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem key={'MSCR2'} disablePadding>
-                <ListItemButton>
-                  {/* <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon> */}
-                  {/* <ListItemText primary={'Resource 1'}/> */}
-                  <ListItemText primary={`Resource 2: ${data?.original?.['M/S/C_Resource2']}`} />
-                </ListItemButton>
-              </ListItem>
-              
-            </div>
-          ) : (
-            <div>
-              <ListItem key={'M'} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={`M: ${data?.original?.M}`} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem key={'M/S'} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={`M/S: ${data?.original?.['M/S']}`} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem key={'M/S/C'} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {<MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={`M/S/C: ${data?.original?.['M/S/C']}`} />
-                </ListItemButton>
-              </ListItem>
-            </div>
-          )
-        }
         <Divider />
-        {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))} */}
       </List>
     </Box>
   );
