f67d1472c2a174e06dfd74c076ee39a01634c7af
anujshashimal1bt <anuj.jayasekara@1billiontech.com>
2023-11-22 13:06:42 +0530

    updated

--- seerDiscoverDataTableROMPCF/src/Utils/SidePaneUtils/EstimateAverageRate/CommonUtils/commonLogic/index.tsx
+++ seerDiscoverDataTableROMPCF/src/Utils/SidePaneUtils/EstimateAverageRate/CommonUtils/commonLogic/index.tsx
@@ -21,13 +21,13 @@ export const commonSidePaneLogic = (selectorForSidePane: any, columnValue: any) 
             }
         });
         const rowsArray = []
-        if (baseValue) rowsArray.push({ name: "Module Estimate value", value: baseValue, rowColor: "#E0E0E0", align: "left" });
+        if (baseValue) rowsArray.push({ name: "Module Estimate value", value: `£${baseValue}`, rowColor: "#E0E0E0", align: "left" });
         if ((uniqueData_resultModule?.length + uniqueData_resultOverideModule?.length)) rowsArray.push({ name: "Number of Modules", value: uniqueData_resultModule?.length + uniqueData_resultOverideModule?.length, rowColor: "#E0E0E0", align: "left" });
         if (resultOverideModule?.length) rowsArray.push({ name: "Module Overrides", rowColor: "#E0E0E0" });
         if (resultOverideModule?.length) rowsArray.push(...resultOverideModule);
-        if (moduleValue) rowsArray.push({ name: "Requirements Estimate value", value: moduleValue, rowColor:"#E0E0E0" , align: "left"});
-        if ((uniqueData_resultBase?.length + uniqueData_resultOverideBase?.length)) rowsArray.push({ name: "Number of Requirements", value: uniqueData_resultBase?.length + uniqueData_resultOverideBase?.length , rowColor: "#E0E0E0" , align: "left"  });
-        if (uniqueData_resultOverideBase?.length) rowsArray.push({ name: "Number of Requirement overrides", value: uniqueData_resultOverideBase?.length , rowColor: "#E0E0E0", align: "left"  });
+        if (moduleValue) rowsArray.push({ name: "Requirements Estimate value", value: `£${moduleValue}`, rowColor: "#E0E0E0", align: "left" });
+        if ((uniqueData_resultBase?.length + uniqueData_resultOverideBase?.length)) rowsArray.push({ name: "Number of Requirements", value: uniqueData_resultBase?.length + uniqueData_resultOverideBase?.length, rowColor: "#E0E0E0", align: "left" });
+        if (uniqueData_resultOverideBase?.length) rowsArray.push({ name: "Number of Requirement overrides", value: uniqueData_resultOverideBase?.length, rowColor: "#E0E0E0", align: "left" });
 
         return rowsArray
     }
